9a3014c7e5f408ffadee370c673fcb6fa5a513eb
Малкова Анна <62211119+alikawhite@users.noreply.github.com>
2024-03-25 12:16:36 +0300

    Delete Java_Bootcamp.Day04-1-develop directory

--- Java_Bootcamp.Day04-1-develop/src/ex00/ImagesToChar/src/edu/school21/printer/logic/Controller.java
+++
@@ -1,1 +0,0 @@
-package edu.school21.printer.logic;public class Controller {    public static void paint(String pathToFile, char a, char b) {        Painter.printMass(Parser.parseFile(pathToFile), a, b);    }}
--- Java_Bootcamp.Day04-1-develop/src/ex01/ImagesToChar/src/edu/school21/printer/logic/Controller.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.printer.logic;
-
-public class Controller {
-    public static void paint(char a, char b) {
-        Painter.printMass(Parser.parseFile(), a, b);
-    }
-}
